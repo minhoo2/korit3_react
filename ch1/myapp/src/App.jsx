36a0b1eb438b843bb8ee073b15b765fa9c5668a7
minhoo2 <zhf1066@naver.com>
2025-05-20 12:37:50 +0900

    feat: 20250520 JS review

---
+++ ch1/myapp/src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <>
+      <h1>Hello, React!</h1>
+      <h2>첫 번째 Component</h2>
+      <App2/>  
+      <App3/>
+    </>
+  )
+}
+
+function App2() {
+  return(
+    <>
+      <h1>안녕하세요.</h1>
+    </>
+  )
+}
+
+function App3() {
+  return(
+    <>
+      <h1>반갑습니다.</h1>
+    </>
+  )
+}
+
+export default App
