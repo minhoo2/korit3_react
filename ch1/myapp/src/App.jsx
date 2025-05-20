1f2ee02e06bdfc83b5d7fcb46446807923b1eab3
minhoo2 <zhf1066@naver.com>
2025-05-20 15:46:14 +0900

    commit

--- ch1/myapp/src/App.jsx
+++ ch1/myapp/src/App.jsx
@@ -1,10 +1,32 @@
+import Hello from "./Hello"
+import Hello2 from "./Hello2"
+import Hello3 from "./Hello3"
+
+import './App.css';
+
 function App() {
+  const divStyle = { color: 'tomato', height: 50 }
+
+
   return (
     <>
       <h1>Hello, React!</h1>
       <h2>첫 번째 Component</h2>
-      <App2/>  
-      <App3/>
+      <App2 />
+      <div style={{ height : 20, width: 200, color: 'red'}}>
+        점심 메뉴는 무엇이었나요?
+      </div>
+      <div style={divStyle}>
+        저는 쌀과자, 연탄빵, 커피를 먹었습니다.
+      </div>
+      
+      <div className="App-header">
+        링크 CSS 적용
+      </div>
+      <Hello />
+      <Hello2 firstName='김' lastName='이'/>
+      <Hello2 firstName='김' lastName='삼'/>
+      <Hello3 firstName='사' lastName='김'/>
     </>
   )
 }
@@ -16,13 +38,4 @@ function App2() {
     </>
   )
 }
-
-function App3() {
-  return(
-    <>
-      <h1>반갑습니다.</h1>
-    </>
-  )
-}
-
 export default App
---
+++ ch1/myapp/src/Hello3.jsx
@@ -0,0 +1,6 @@
+function Hello3({firstName, lastName}) {
+  return <h2>안녕하세요, {lastName} {firstName} </h2>;
+}
+
+
+export default Hello3;
