344f9e010a625bcc5ed62511d878794c32b5dee2
minhoo2 <zhf1066@naver.com>
2025-05-27 13:24:41 +0900

    feat: 20250527

--- ch5/shoppinglist/src/App.tsx
+++ ch5/shoppinglist/src/App.tsx
@@ -1,13 +1,49 @@
+import { useState } from 'react'
+import Container from '@mui/material/Container'
+import { AppBar } from '@mui/material'  // 2번 라인은 구조분해x 3번 라인부터 구조 분해
+import { Toolbar } from '@mui/material'
+import { Typography } from '@mui/material'
+
+import { List, ListItem, ListItemText } from '@mui/material'
+
+import AddItem from './components/AddItem'
 
 import './App.css'
 
-function App() {
+export type Item = {
+  product: string;
+  amount: string;
+}
 
+function App() {
+  const [ items, setitems ] = useState<Item[]>([]);
+
+  const addItem = (item: Item) => {
+    setitems([item, ...items]);
+  }
 
   return (
-    <>
+    <Container>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant="h6">
+          Shopping List / 쇼핑 리스트
+          </Typography>
+        </Toolbar>
+      </AppBar>
 
-    </>
+      <AddItem addItem={addItem}/>
+
+      <List>
+        {
+          items.map((item, index) => 
+            <ListItem key={index} divider>
+              <ListItemText primary={item.product} secondary={item.amount} />
+            </ListItem>
+          )
+        }
+      </List>
+    </Container>
   )
 }
 
--- ch5/shoppinglist/src/main.tsx
+++ ch5/shoppinglist/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+// import './index.css'   -> mui 사용을 위해 주석처리
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
