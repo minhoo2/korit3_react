e5f718f91a15120f19f499ea1eb537ca4fc98710
minhoo2 <zhf1066@naver.com>
2025-05-28 15:47:05 +0900

    commit

--- ch6/carfront/src/App.tsx
+++ ch6/carfront/src/App.tsx
@@ -1,12 +1,25 @@
+import { AppBar, Toolbar, Typography, Container, CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Carlist from "./components/Carlist";
 
+const queryClient = new QueryClient();
 
 function App() {
 
-
   return (
-    <>
-
-    </>
+    <Container maxWidth="xl">
+    <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Car Shop | 자동차 거래소
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <QueryClientProvider client={queryClient}>
+        <Carlist />
+      </QueryClientProvider>
+    </Container>
   )
 }
 
